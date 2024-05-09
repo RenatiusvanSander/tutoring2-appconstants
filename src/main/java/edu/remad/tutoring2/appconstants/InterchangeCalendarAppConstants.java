33f9c997ce524b402c0aadf907b394f1883542c8
Remy Meier <remad@web.de>
2024-05-09 18:54:35 +0200

    master: Constants for InterchangeCalendar defined for emails

---
+++ src/main/java/edu/remad/tutoring2/appconstants/InterchangeCalendarAppConstants.java
@@ -0,0 +1,14 @@
+package edu.remad.tutoring2.appconstants;
+
+public final class InterchangeCalendarAppConstants {
+	
+	private InterchangeCalendarAppConstants() {}
+	
+	public static final String FILE_SEPARATOR = ".";
+	
+	public static final String INTERCHANGE_CALENDAR_FILENAME = "calendar_entry";
+	
+	public static final String INTERCHANGE_CALENDAR_FILENAME_EXTENSION = "ics";
+	
+	public static final String INTERCHANGE_CALENDAR_FILENAME_WITH_EXTENSION = INTERCHANGE_CALENDAR_FILENAME + FILE_SEPARATOR + INTERCHANGE_CALENDAR_FILENAME_EXTENSION;
+}
