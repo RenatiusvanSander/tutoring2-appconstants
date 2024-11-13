ea539907bd8148bb1777e2ee1ecfb1655cd3c5f6
RenatiusvanSander <remad@web.de>
2024-11-13 20:59:00 +0100

    master: constants for XRechnung_Sender

---
+++ src/main/java/edu/remad/tutoring2/appconstants/XRechnungAppConstants.java
@@ -0,0 +1,30 @@
+package edu.remad.tutoring2.appconstants;
+
+public final class XRechnungAppConstants {
+
+	private XRechnungAppConstants() {
+		// do not instantiate
+	}
+	
+	public static final String XRECHNUNG_SENDER_FULLNAME = "xRechnungSenderFullname";
+	
+	public static final String XRECHNUNG_SENDER_EMAIL = "xRechnungSenderEmail";
+	
+	public static final String XRECHNUNG_SENDER_STREET_AND_HOUSE_NO = "xRechnungSenderStreetAndHouseNo";
+	
+	public static final String XRECHNUNG_SENDER_PHONE = "xRechnungSenderPhone";
+	
+	public static final String XRECHNUNG_SENDER_ZIP = "xRechnungSenderZip";
+	
+	public static final String XRECHNUNG_SENDER_ZIP_LOCATION = "xRechnungSenderZipLocation";
+	
+	public static final String XRECHNUNG_SENDER_BANK_DETAILS_IBAN = "xRechnungSenderBankDetailsIban";
+	
+	public static final String XRECHNUNG_SENDER_BANK_DETAILS_BIC = "xRechnungSenderBankDetailsBic";
+	
+	public static final String XRECHNUNG_SENDER_TAX_ID = "xRechnungSenderTaxId";
+	
+	public static final String XRECHNUNG_SENDER_VAT_ID = "xRechnungSenderVatId";
+	
+	public static final String XRECHNUNG_SENDER_LEITWEG_ID = "xRechnungSenderLeitwegId";
+}
