62687ffc6a197d00bc39eaa7d8a555345bfcbcc0
Remy Meier <remad@web.de>
2024-11-19 20:38:26 +0100

    master: classpath and java doc added to XRechnung App Constants

--- src/main/java/edu/remad/tutoring2/appconstants/XRechnungAppConstants.java
+++ src/main/java/edu/remad/tutoring2/appconstants/XRechnungAppConstants.java
@@ -6,31 +6,45 @@ public final class XRechnungAppConstants {
 		// do not instantiate
 	}
 	
+	/** XRechnung property key for sender's full name*/
 	public static final String XRECHNUNG_SENDER_FULLNAME = "xRechnungSenderFullname";
 	
+	/** XRechnung property key for sender's e-mail */
 	public static final String XRECHNUNG_SENDER_EMAIL = "xRechnungSenderEmail";
 	
+	/** XRechnung property key for sender's street and house number */
 	public static final String XRECHNUNG_SENDER_STREET_AND_HOUSE_NO = "xRechnungSenderStreetAndHouseNo";
 	
+	/** XRechnung property key for sender's phone */
 	public static final String XRECHNUNG_SENDER_PHONE = "xRechnungSenderPhone";
 	
+	/** XRechnung property key for sender's Zip */
 	public static final String XRECHNUNG_SENDER_ZIP = "xRechnungSenderZip";
 	
+	/** XRechnung property key for sender's zip location */
 	public static final String XRECHNUNG_SENDER_ZIP_LOCATION = "xRechnungSenderZipLocation";
 	
+	/** XRechnung property key for sender's Bank Details IBAN */
 	public static final String XRECHNUNG_SENDER_BANK_DETAILS_IBAN = "xRechnungSenderBankDetailsIban";
 	
+	/** XRechnung property key for sender's Bank Details BIC */
 	public static final String XRECHNUNG_SENDER_BANK_DETAILS_BIC = "xRechnungSenderBankDetailsBic";
 	
+	/** XRechnung property key for sender's tax id */
 	public static final String XRECHNUNG_SENDER_TAX_ID = "xRechnungSenderTaxId";
 	
+	/** XRechnung property key for sender's VAT Id */
 	public static final String XRECHNUNG_SENDER_VAT_ID = "xRechnungSenderVatId";
 	
+	/** XRechnung property key for sender's Leitweg-Id */
 	public static final String XRECHNUNG_SENDER_LEITWEG_ID = "xRechnungSenderLeitwegId";
 	
+	/** XRechnung property key for sender's country code */
 	public static final String XRECHNUNG_SENDER_COUNTRY_CODE = "xRechnungSenderCountryCode";
 	
+	/** XRechnung property key for sender's country */
 	public static final String XRECHNUNG_SENDER_COUNTRY = "xRechnungSenderCountry";
 	
+	/** XRechnung property key for recipient's custom VAT in percent */
 	public static final String XRECHNUNG_RECIPIENT_CUSTOM_VAT = "xRechnungRecipientCustomVat";
 }
